57ca38c01dd3e4f8b60baf8f8c69dce67ea209d1
tushar8871 <tusharkhachane6@gmail.com>
2021-11-06 13:59:41 +0530

    Update to Deploy

--- push_ecr.sh
+++ push_ecr.sh
@@ -3,7 +3,7 @@ REGION=us-east-1
 ECR_URL=671761574982.dkr.ecr.us-east-1.amazonaws.com/capstone
 
 # log docker into eks using aws cli
-$(aws ecr get-login --no-include-email --region $REGION)
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 671761574982.dkr.ecr.us-east-1.amazonaws.com
 # build a docker image with a unique build number
 docker build -t $ECR_URL  .
 
